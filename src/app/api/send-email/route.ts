02cba16bc5934fb820c463241f5cc6bbc9808bac
jhann <jhangomez25@gmail.com>
2025-08-09 19:45:12 -0500

    Se agrego contenido relacionado a cloudflare v6

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -3,6 +3,8 @@ import { Resend } from 'resend';
 import { EmailToClient } from '@/components/emails/email-to-client';
 import { EmailToOwner } from '@/components/emails/email-to-owner';
 
+export const runtime = 'edge';
+
 const fromEmail = 'Contacto Harkaysoft <jhan@harkaysoft.com>';
 const ownerEmail = 'jhangomez25@gmail.com'; 
 
