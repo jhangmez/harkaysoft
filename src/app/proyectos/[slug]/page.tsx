a6b27e7d2bd4430ee9968a75ba790a968e2620bd
jhann <jhangomez25@gmail.com>
2025-08-09 19:23:18 -0500

    Se agrego contenido relacionado a cloudflare v4

--- src/app/proyectos/[slug]/page.tsx
+++ src/app/proyectos/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import { notFound } from "next/navigation";
-import Image from "next/image";
 import Link from "next/link";
 import proyectos from "@/data/proyectos.json";
 import { Button } from "@/components/ui/button";
