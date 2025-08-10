4ff5e56462fe839f39282af0f846b4315b33b146
jhann <jhangomez25@gmail.com>
2025-08-09 19:04:30 -0500

    Se agrego contenido relacionado a cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+        pathname: '/f/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'vrf137hlqv.ufs.sh',
+        port: '',
+        pathname: '/f/**'
+      },
+    ]
+  }
 };
-
+if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform();
+  }
 export default nextConfig;
